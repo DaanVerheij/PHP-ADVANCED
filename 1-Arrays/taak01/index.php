af082342424b60710c0a8e72725652592a353e88
Daan <daan.verheij@student.rocva.nl>
2021-02-01 11:36:04 +0100

    taak 01

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$student = array(
+    "voornaam " =>"Mohammad",
+    "achternaam " =>"Ali",
+    "klas " =>"0A",
+    "leeftijd " =>"16"
+);
+
+echo "Gegevens Mohammad: Voornaam: ". $student["voornaam "]. " Achternaam: ". $student["achternaam "]. " Leeftijd: ". $student["leeftijd "]. " Klas: ". $student["klas "];
+?>
+
+</body>
+</html> 
---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<ul>
+<?php
+$leeftijd = 0;
+
+while($leeftijd < 18){
+    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen.<br />";
+    $leeftijd++;
+
+}
+
+if($leeftijd = 18){
+
+    echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+}
+
+?>
+</ul>
+
+</body>
+</html> 

9ea0d749f0494911c7d9212ffd02ed0311e75812
Daan <daan.verheij@student.rocva.nl>
2021-02-01 11:46:55 +0100

    taak 02

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+for($x=0; $x < 18; $x++){
+    echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br />";
+}
+
+
+if($x = 18){
+    echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+}
+
+?>
+
+
+
+
+</body>
+</html> 

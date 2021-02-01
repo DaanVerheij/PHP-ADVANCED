cfa4e5f4a402c4910ca813f9cb47d9843143b74a
Daan <daan.verheij@student.rocva.nl>
2021-02-01 12:02:30 +0100

    taak 03

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+foreach($dagen as $waarde){
+    echo "$waarde is een dag in de week<br />";
+
+}
+?>
+
+
+
+
+</body>
+</html> 

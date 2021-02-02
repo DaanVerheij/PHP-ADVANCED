eee486bd15bb0cbe57229d62b4538671bf169d5c
Daan <daan.verheij@student.rocva.nl>
2021-02-02 12:00:27 +0100

    taak 05

---
+++ 2-Loops/taak05/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Welkom " .$_POST["voornaam"];
+    echo " " .$_POST["achternaam"];
+    echo "<br />Leeftijd: " .$_POST["leeftijd"];
+    echo "<br />Adres: " .$_POST["adres"];
+    echo "<br />Plaatsnaam: " .$_POST["plaatsnaam"];
+
+    ?>
+
+</body>
+</html>

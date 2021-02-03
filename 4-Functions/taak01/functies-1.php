1d4531ab0ac23eb06e9fbe10c7829a7930ba5a66
Daan <daan.verheij@student.rocva.nl>
2021-02-03 12:06:18 +0100

    functions taak 01

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+function schrijfKleur($red) {
+    echo "Deze kleur is $red<br>";
+  }
+  
+  schrijfKleur("green");
+  schrijfKleur("blue");
+  schrijfKleur("orange");;
+  ?>
+
+
+
+
+
+
+
+</body>
+</html>

a53fdefc879bfc32d758e77965f24eec5014300a
Daan <daan.verheij@student.rocva.nl>
2021-02-03 10:59:49 +0100

    forms taak 03

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Artiest: " .$_GET["artiest"];
+    echo "<br />Titel: " .$_GET["titel"];
+    echo "<br />Jaar van uitkomst: " .$_GET["jaar"];
+    echo "<br />Muzieksoort: " .$_GET["soort"];
+
+   
+    ?>
+
+
+</body>
+</html>

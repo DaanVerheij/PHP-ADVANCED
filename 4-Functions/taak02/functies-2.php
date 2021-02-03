ba15cc7cf573aa7b44c2a09d8bb3ee70ed0e92ba
Daan <daan.verheij@student.rocva.nl>
2021-02-03 12:36:09 +0100

    functions taak02

---
+++ 4-Functions/taak02/functies-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function rekenUit($getal1, $getal2, $getal3){
+        echo "Als ik de getallen $getal1, $getal2 en $getal3 bij elkaar optel, is de uitkomst 15";
+
+    }
+
+$getal1 = 5;
+$getal2 = 4;
+$getal3 = 6;
+ rekenUit($getal1, $getal2, $getal3);
+
+
+    ?>
+
+
+
+</body>
+</html>
